2539532ae9b5d55a00cf3a7615997a6bd8494c90
Sumed Tamang <xumed001@gmail.com>
2023-11-13 13:12:42 -0600

    updated render code

--- quizzical_React/src/Components/GameScreen.jsx
+++ quizzical_React/src/Components/GameScreen.jsx
@@ -3,18 +3,13 @@ import he from "he"
 
 export default function GameScreen() {
     const URL = "https://opentdb.com/api.php?amount=5&category=15&difficulty=easy&type=multiple"
-    let uKey = 0
+    console.log('Component rendered')
     const [triviaData, setTriviaData] = useState(null)
     const [loading, setLoading] = useState(true)
-    const [gameAns, setGameAns] = useState({
-        answer0: '',
-        answer1: '',
-        answer2: '',
-        answer3: '',
-        answer4: ''
-    })
+    let userAns = new Array(4)
 
     useEffect(() => {
+        console.log('Effect ran')
         fetch(URL)
             .then((res) => res.json())
             .then((data) => {
@@ -23,14 +18,9 @@ export default function GameScreen() {
             })
     }, [])
 
-    function handleChange(event) {
-        const {name, value, checked} = event.target
-        setGameAns(iniData => ({
-            ...iniData,
-            [name]: value
-        }))
-    }   
-
+    const correctAns = triviaData?.map((item) => he.decode(item.correct_answer))
+    console.log(correctAns)
+    
     const triviaAns = triviaData?.map((ans) => {
         let randomIndex = Math.floor(Math.random() * 4)
         const allAns = ans.incorrect_answers
@@ -38,78 +28,89 @@ export default function GameScreen() {
         return allAns
     })
 
-    console.log(triviaAns[0][0])
+    function handleChange(event) {
+        console.log(event.target.value)
+        userAns[event.target.name] = event.target.value
+    }   
 
-    const triviaQnA = triviaData?.map((item) => {
-        let randomIndex = Math.floor(Math.random() * 4)
-        const allAns = item.incorrect_answers
-        allAns.splice(randomIndex, 0, `${item.correct_answer}`)
+    function compareAns(a, b) {
+        let score = 0
+        for(let i=0; i<a.length; i++) {
+            if(a[i] === b[i]){
+                score+=1
+            }
+        }
+        return score
+    }
 
-        let lableName = `answer${uKey}`
-        uKey += 1
+    function checkAnswers(){
+        console.log(compareAns(userAns, correctAns))
+    }
 
-        return (
-            <>
-                <p className="game--question">{he.decode(item.question)}</p>
-                <ul className="game-screen--ans">
-                    <li>
-                        <input 
-                            onChange={handleChange} 
-                            type="radio" 
-                            name={lableName} 
-                            value={he.decode(allAns[0])} 
-                            checked={gameAns.lableName === he.decode(allAns[0])}
-                        />
-                        <label>
-                            {he.decode(allAns[0])}
-                        </label>
-                    </li>
+    // const triviaQnA = triviaData?.map((item) => {
+    //     let randomIndex = Math.floor(Math.random() * 4)
+    //     const allAns = item.incorrect_answers
+    //     allAns.splice(randomIndex, 0, `${item.correct_answer}`)
 
-                    <li>
-                        <input 
-                            onChange={handleChange} 
-                            type="radio" 
-                            name={lableName} 
-                            value={he.decode(allAns[1])} 
-                            checked={gameAns.lableName === he.decode(allAns[1])}
-                        />
-                        <label>
-                            {he.decode(allAns[1])}
-                        </label>
-                    </li>
+    //     return (
+    //         <>
+    //             <p className="game--question">{he.decode(item.question)}</p>
+    //             <ul className="game-screen--ans">
+    //                 <li key={1}>
+    //                     <input 
+    //                         // onChange={handleChange} 
+    //                         type="radio" 
+    //                         // name={lableName} 
+    //                         value={he.decode(allAns[0])} 
+    //                         // checked={gameAns.lableName === he.decode(allAns[0])}
+    //                     />
+    //                     <label>
+    //                         {he.decode(allAns[0])}
+    //                     </label>
+    //                 </li>
 
-                    <li>
-                        <input 
-                            onChange={handleChange} 
-                            type="radio" 
-                            name={lableName} 
-                            value={he.decode(allAns[2])} 
-                            checked={gameAns.lableName === he.decode(allAns[2])}
-                        />
-                        <label>
-                            {he.decode(allAns[2])}
-                        </label>
-                    </li>
+    //                 <li key={2}>
+    //                     <input 
+    //                         // onChange={handleChange} 
+    //                         type="radio" 
+    //                         // name={lableName} 
+    //                         value={he.decode(allAns[1])} 
+    //                         // checked={gameAns.lableName === he.decode(allAns[1])}
+    //                     />
+    //                     <label>
+    //                         {he.decode(allAns[1])}
+    //                     </label>
+    //                 </li>
+
+    //                 <li key={3}>
+    //                     <input 
+    //                         // onChange={handleChange} 
+    //                         type="radio" 
+    //                         // name={lableName} 
+    //                         value={he.decode(allAns[2])} 
+    //                         // checked={gameAns.lableName === he.decode(allAns[2])}
+    //                     />
+    //                     <label>
+    //                         {he.decode(allAns[2])}
+    //                     </label>
+    //                 </li>
                     
-                    <li>
-                        <input 
-                            onChange={handleChange} 
-                            type="radio" 
-                            name={lableName} 
-                            value={he.decode(allAns[3])} 
-                            checked={gameAns.lableName === he.decode(allAns[3])}
-                        />
-                        <label>
-                            {he.decode(allAns[3])}
-                        </label>
-                    </li>
-                </ul>
-            </>
-        )
-    })
-
-    console.log(triviaData)
-    // console.log(gameAns)
+    //                 <li key={4}>
+    //                     <input 
+    //                         // onChange={handleChange} 
+    //                         type="radio" 
+    //                         // name={lableName} 
+    //                         value={he.decode(allAns[3])} 
+    //                         // checked={gameAns.lableName === he.decode(allAns[3])}
+    //                     />
+    //                     <label>
+    //                         {he.decode(allAns[3])}
+    //                     </label>
+    //                 </li>
+    //             </ul>
+    //         </>
+    //     )
+    // })
 
     return (
         <div className="game-screen-main">
@@ -118,7 +119,58 @@ export default function GameScreen() {
                     'Loading...'
                 ) : (
                     <div className="game-screen--qna">
-                        {triviaQnA}
+                        {triviaData.map((qanda, index) => <>
+                        <p key={qanda.correct_answer}>{he.decode(qanda.question)}</p>
+                        <ul className="game-screen--ans">
+                            <li>
+                                <input 
+                                    type="radio" 
+                                    name={index}
+                                    onChange={handleChange}
+                                    value={he.decode(triviaAns[index][0])}
+                                 />
+                                <label>
+                                    {he.decode(triviaAns[index][0])}
+                                </label>
+                            </li>
+                            <li>
+                                <input 
+                                    type="radio" 
+                                    name={index}
+                                    onChange={handleChange}
+                                    value={he.decode(triviaAns[index][1])}
+                                />
+                                <label>
+                                    {he.decode(triviaAns[index][1])}
+                                </label>
+                            </li>
+                            <li>
+                                <input 
+                                    type="radio" 
+                                    name={index}
+                                    onChange={handleChange}
+                                    value={he.decode(triviaAns[index][2])}
+                                />
+                                <label>
+                                    {he.decode(triviaAns[index][2])}
+                                </label>
+                            </li>
+                            <li>
+                                <input 
+                                    type="radio" 
+                                    name={index}
+                                    onChange={handleChange}
+                                    value={he.decode(triviaAns[index][3])}
+                                />
+                                <label>
+                                    {he.decode(triviaAns[index][3])}
+                                </label>
+                            </li>
+                        </ul>
+                        </>)}
+                        <button onClick={checkAnswers}>
+                            Check answers
+                        </button>
                     </div>
                 )
             }   
