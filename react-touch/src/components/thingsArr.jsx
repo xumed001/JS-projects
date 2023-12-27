435dd43894b0bc880d9a87fa42e5d0ed638e6fee
Sumed Tamang <xumed001@gmail.com>
2023-12-27 17:26:28 -0600

    test

---
+++ react-touch/src/components/thingsArr.jsx
@@ -0,0 +1,19 @@
+import {useState} from "react";
+
+export default function Thing() {
+    const [thingsArray, setThingsArray] = useState(['Thing 1', 'Thing 2'])
+    
+    function addItem() {
+        let addToArr = `Thing ${thingsArray.length + 1}`
+        setThingsArray([...thingsArray, addToArr])
+    }
+    
+    const thingsElements = thingsArray.map(thing => <p key={thing}>{thing}</p>)
+    
+    return (
+        <div>
+            <button onClick={addItem} >Add Item</button>
+            {thingsElements}
+        </div>
+    )
+}
